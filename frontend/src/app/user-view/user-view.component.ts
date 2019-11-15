34b7cf08f7bb06fbcb3f34603dea13dede935726
Lukasz <lukasz.manterys@gmail.com>
2019-11-15 23:44:58 +0100

    full support for paginator on frontend side

--- frontend/src/app/user-view/user-view.component.ts
+++ frontend/src/app/user-view/user-view.component.ts
@@ -2,7 +2,7 @@ import { UserNumberService } from './../services/user-number.service';
 import { User } from './../model/user';
 import { UserService } from './../services/user.service';
 import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatSort, MatTableDataSource, PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-user-view',
@@ -11,52 +11,23 @@ import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 })
 export class UserViewComponent {
 
+  private dataSource: User[];
+  private initialPageIndex = 0;
+  private pageSize = 10;
+  private totalLength = 100;
 
-  private users: User[] = [];
-  private usersPage: User[] = [];
   private allUserNumber = 0;
   private loggedUserNumber = 0;
-  usery: User[] = [
-    {id: 1, firstName: 'luki', secondName: 'monte', email: 'luki.monte@gmail.com', timestamp: 123, timestampDate: '15 Nov 2019'},
-    {id: 2, firstName: 'kamil', secondName: 'mante', email: 'luki.monte@gmail.com', timestamp: 124, timestampDate: '14 Nov 2019'}
-  ];
 
   displayedColumns = ['name', 'surename', 'email', 'timestamp', 'logged'];
-  dataSource;
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor(private userService: UserService,
               private userNumberService: UserNumberService) {
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(this.users);
-    this.dataSource = this.usery;
   }
 
   ngOnInit() {
-
-    console.log('UserViewComponent::ngOnInit()');
-
-    this.userService.findUserPage('0', '8').subscribe(data => {
-        this.usersPage = data;
-
-        for (const u of this.usersPage) {
-          console.log('User page received in user-view: ' + u);
-        }
-    });
-
-    // this.userService.findAll().subscribe(data => {
-
-    //     this.users = data;
-    //     for (const user of this.users) {
-    //       user.timestampDate = (new Date(user.timestamp)).toLocaleString();
-    //     }
-
-    //     this.dataSource = new MatTableDataSource(data);
-    //     this.dataSource.paginator = this.paginator;
-    //   });
 
     this.userNumberService.getAllUserNumber().subscribe(num => {
       this.allUserNumber = num;
@@ -67,9 +38,24 @@ export class UserViewComponent {
     });
   }
 
+  getUserData(event: PageEvent) {
+
+    this.userService.findUserPage(String(event.pageIndex), String(event.pageSize)).subscribe(data => {
+      this.dataSource = data;
+    });
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    console.log('The basic params of paginator are being filled');
+
+    this.userService.findUserPage(String(this.initialPageIndex), String(this.pageSize)).subscribe(data => {
+      this.dataSource = data;
+    });
+
+    this.paginator.pageIndex = this.initialPageIndex;
+    this.paginator.pageSize = this.pageSize;
+    this.paginator.length = this.totalLength;
   }
 }
 
